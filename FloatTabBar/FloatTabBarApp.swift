27130e4de9ac0ccfc839dbd61c47efce7655933a
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-08-20 21:21:50 +0500

    Initial Commit

---
+++ FloatTabBar/FloatTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatTabBarApp.swift
+//  FloatTabBar
+//
+//  Created by Benji Loya on 20.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FloatTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
